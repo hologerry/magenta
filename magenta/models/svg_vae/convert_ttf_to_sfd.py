7953bcde8135054b3ae3c3ff90e5bb70df65c14c
Gerry Gao <hologerry@gmail.com>
2020-05-17 09:03:49 +0000

    update convert sfd

--- magenta/models/svg_vae/convert_ttf_to_sfd.py
+++ magenta/models/svg_vae/convert_ttf_to_sfd.py
@@ -22,6 +22,16 @@ for font_line in valid_fonts_urls:
         cur_font = fontforge.open(font_file_path)                # Open a font
     except Exception as e:
         print('Cannot open', font_name)
+        print(e)
+        continue
+
+    try:
+        # test all char exist
+        cur_font.selection.select(('ranges', None), 'A', 'Z')
+        cur_font.selection.select(('ranges', None), 'a', 'z')
+    except Exception as e:
+        print(font_name, 'does not have all chars')
+        print(e)
         continue
 
     font_id_split_name.write("{:06d}".format(font_id) + ', ' + split + ', ' + font_name + '\n')
@@ -33,11 +43,11 @@ for font_line in valid_fonts_urls:
     for char_id, char in enumerate(alphabet_chars):
         char_description = open(os.path.join(target_dir, '{:06d}_{:02d}.txt'.format(font_id, char_id)), 'w')
 
-        cur_font.selection.select(('ranges', None), char, char)
+        cur_font.selection.select(char)
         cur_font.copy()
 
         new_font_for_char = fontforge.font()
-        new_font_for_char.selection.select(('ranges', None), char, char)
+        new_font_for_char.selection.select(char)
         new_font_for_char.paste()
         new_font_for_char.fontname = "{:06d}_".format(font_id) + font_name
 
